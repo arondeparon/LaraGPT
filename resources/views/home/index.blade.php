e8427a343d7244706cb5319182644d571ef6d76d
Aron Rotteveel <hi@aron.codes>
2023-03-10 17:50:23 +0100

    cleanup

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,28 +2,33 @@
 
 @section('content')
     <div class="flex">
-        <div class="w-1/5 h-screen bg-gray-800 text-white">
-            <a href="{{ route('reset') }}"
-               class="flex space-x-8 border border-white rounded m-2 p-4 items-center hover:bg-gray-700">
-                <!-- svg "plus" icon -->
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
-                </svg>
-                <div>New chat</div>
-            </a>
-            @if ($conversations)
-                <ul class="flex flex-col m-2">
-                    @foreach ($conversations as $conversation)
-                        <li>
-                            <a href="/conversation/{{ $conversation['id'] }}"
-                               title="{{ $conversation['summary'] }} }}"
-                               class="block p-4 rounded hover:bg-gray-700 truncate overflow-hidden w-80"
-                            >{{ $conversation['summary'] }}</a>
-                        </li>
-                    @endforeach
-                </ul>
-            @endif
+        <div class="w-1/5 h-screen bg-gray-800 text-white overflow-y-scroll">
+            <div class="h-4/5">
+                <a href="{{ route('reset') }}"
+                   class="flex space-x-8 border border-white rounded m-2 p-4 items-center hover:bg-gray-700">
+                    <!-- svg "plus" icon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                         stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
+                    </svg>
+                    <div>New chat</div>
+                </a>
+                @if ($conversations)
+                    <ul class="flex flex-col m-2">
+                        @foreach ($conversations as $conversation)
+                            <li>
+                                <a href="/conversation/{{ $conversation['id'] }}"
+                                   title="{{ $conversation['summary'] }} }}"
+                                   class="block p-4 rounded hover:bg-gray-700 truncate overflow-hidden w-80"
+                                >{{ $conversation['summary'] }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
+            </div>
+            <div>
+
+            </div>
         </div>
         <div class="w-4/5 h-screen relative">
             <div class="flex flex-col h-full w-full relative">
@@ -51,9 +56,7 @@
                         <label for="message" class="hidden">Prompt:</label>
                         <textarea id="message" type="text" name="message" autocomplete="off"
                                   class="border rounded-md  p-2 flex-1"></textarea>
-                        <button type="submit" class="bg-blue-500 text-white p-2 rounded-md">Send</button>
-                        <a class="bg-gray-800 text-white p-2 rounded-md" href="{{ route('reset') }}">Reset
-                            Conversation</a>
+                        <button type="submit" class="bg-blue-600 text-white p-2 rounded-md">Send</button>
                     </form>
                 </div>
             </div>
